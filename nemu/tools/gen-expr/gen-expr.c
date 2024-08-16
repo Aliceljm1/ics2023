d202a2f09671eb8ac791dedf9cfe0badff2ae0f4
ljm <yhxljm@qq.com>
2024-08-16 10:12:01 +0800

    新增随机生成表达式的功能，目前问题是依然存在复杂运算除0, overflow的问题

--- nemu/tools/gen-expr/gen-expr.c
+++ nemu/tools/gen-expr/gen-expr.c
@@ -1,17 +1,17 @@
 /***************************************************************************************
-* Copyright (c) 2014-2022 Zihao Yu, Nanjing University
-*
-* NEMU is licensed under Mulan PSL v2.
-* You can use this software according to the terms and conditions of the Mulan PSL v2.
-* You may obtain a copy of Mulan PSL v2 at:
-*          http://license.coscl.org.cn/MulanPSL2
-*
-* THIS SOFTWARE IS PROVIDED ON AN "AS IS" BASIS, WITHOUT WARRANTIES OF ANY KIND,
-* EITHER EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO NON-INFRINGEMENT,
-* MERCHANTABILITY OR FIT FOR A PARTICULAR PURPOSE.
-*
-* See the Mulan PSL v2 for more details.
-***************************************************************************************/
+ * Copyright (c) 2014-2022 Zihao Yu, Nanjing University
+ *
+ * NEMU is licensed under Mulan PSL v2.
+ * You can use this software according to the terms and conditions of the Mulan PSL v2.
+ * You may obtain a copy of Mulan PSL v2 at:
+ *          http://license.coscl.org.cn/MulanPSL2
+ *
+ * THIS SOFTWARE IS PROVIDED ON AN "AS IS" BASIS, WITHOUT WARRANTIES OF ANY KIND,
+ * EITHER EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO NON-INFRINGEMENT,
+ * MERCHANTABILITY OR FIT FOR A PARTICULAR PURPOSE.
+ *
+ * See the Mulan PSL v2 for more details.
+ ***************************************************************************************/
 
 #include <stdint.h>
 #include <stdio.h>
@@ -22,32 +22,82 @@
 
 // this should be enough
 static char buf[65536] = {};
+#define blen strlen(buf)
+#define bsize sizeof(buf)
 static char code_buf[65536 + 128] = {}; // a little larger than `buf`
 static char *code_format =
-"#include <stdio.h>\n"
-"int main() { "
-"  unsigned result = %s; "
-"  printf(\"%%u\", result); "
-"  return 0; "
-"}";
+    "#include <stdio.h>\n"
+    "int main() { "
+    "  unsigned result = %s; "
+    "  printf(\"%%u\", result); "
+    "  return 0; "
+    "}";
 
-static void gen_rand_expr() {
-  char* temp="(1+2)*(4-3)";
-  memcpy(buf,temp,strlen(temp));
-  // buf[0] = '(1+2)*(4-3)';
+void g_num()
+{
+  int num = rand() % 100;
+  if(num == 0)
+    return g_num();
+  snprintf(buf + blen, sizeof(buf)-blen , "%d", num);
 }
 
-int main(int argc, char *argv[]) {
+void g_left()
+{
+  buf[blen] = '(';
+}
+
+
+void g_right()
+{
+
+  buf[blen]= ')';
+}
+
+void g_opt()
+{
+  char opt = "+-*/"[rand() % 4];
+  buf[blen] = opt;
+}
+
+void g_space()
+{
+  int num = rand() % 3;
+  memcpy(buf + blen, " ", num);
+}
+
+static void gen_rand_expr()
+{
+  int num = rand() % 3;
+  switch (num)
+  {
+  case 0:
+    g_num();
+    break;
+  case 1:
+    g_left();gen_rand_expr();g_right();
+    break;
+  default:
+    gen_rand_expr();
+    g_space();g_opt();g_space();
+    gen_rand_expr();
+    break;
+  }
+}
+
+int main(int argc, char *argv[])
+{
   int seed = time(0);
   srand(seed);
   int loop = 1;
-  if (argc > 1) {
+  if (argc > 1)
+  {
     sscanf(argv[1], "%d", &loop);
   }
   int i;
-  for (i = 0; i < loop; i ++) {
+  for (i = 0; i < loop; i++)
+  {
+    memset(buf, 0, bsize);
     gen_rand_expr();
-
     sprintf(code_buf, code_format, buf);
 
     FILE *fp = fopen("/tmp/.code.c", "w");
@@ -56,7 +106,8 @@ int main(int argc, char *argv[]) {
     fclose(fp);
 
     int ret = system("gcc /tmp/.code.c -o /tmp/.expr");
-    if (ret != 0) continue;
+    if (ret != 0)
+      continue;
 
     fp = popen("/tmp/.expr", "r");
     assert(fp != NULL);
